ed2ddb3c6ea02268f73a773c2f636d0e87628702
xeven95 <xeven95@gmail.com>
2016-11-20 12:46:45 +0100

    Base del proyecto

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: All-PC
+ *
+ * Created on 20 de noviembre de 2016, 12:46
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
